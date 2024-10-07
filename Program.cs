94749c4490f686486c6ca99aabce74a716c697b8
gaspar.balazs <gaspar.balazs@28-DIAK6>
2024-10-07 12:48:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using CA20241007_GB;
+using System.Buffers.Binary;
+using System.Collections.Immutable;
+using System.Text;
+
+Random rnd = new();
+List<Versenyzo> versenyzok = [];
+
+using StreamReader sr = new(@"..\..\..\src\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. feladat: az allomanyban {versenyzok.Count} recept szerepel.");
+
+var f1 = versenyzok.Count(r => r.EletkorKategoria == "elit junior");
+Console.WriteLine($"1. feladat: {f1} versenyző van az elit junior kategóriában.");
+
+var f2 = versenyzok.Average(r => 2024 - r.VersenyzoSzulEv);
+Console.WriteLine($"2. feladat: A versenyzők átlagéletkora: {f2}");
+
+var f3 = versenyzok.Sum(r => r.FutasIdo.Hour);
+Console.WriteLine($"3. feladat: A versenyzők futással töltött összideje: {f3}");
+
+var f4 = versenyzok.Where(r => r.EletkorKategoria == "20-24").Average(r => r.FutasIdo.Hour);
+Console.WriteLine($"4. feladat: Az átlagos úszási idő a 20-24 kategóriában: {f4}");
+
+//var f5 = versenyzok.Where(r => r.VersenyzoNeme == "n").OrderByDescending(r => r.OsszIdo).ToList();
---
+++ Versenyzo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA20241007_GB
+{
+    internal class Versenyzo
+    {
+        public string VersenyzoNev { get; set; }
+        public int VersenyzoSzulEv { get; set; }
+        public int Rajtszam { get; set; }
+        public string VersenyzoNeme { get; set; }
+        public string EletkorKategoria { get; set; }
+        public DateTime UszasIdo { get; set; }
+        public DateTime ElsoDepoIdo { get; set; }
+        public DateTime KerekparIdo { get; set; }
+        public DateTime MasodikDepoIdo { get; set; }
+        public DateTime FutasIdo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{VersenyzoNev} ({VersenyzoSzulEv} éves, {VersenyzoNeme}, {EletkorKategoria}, {UszasIdo}, {ElsoDepoIdo}, {MasodikDepoIdo}, {FutasIdo})";
+        }
+
+        public Versenyzo(string sor)
+        {
+            var v = sor.Split(';');
+            VersenyzoNev = v[0];
+            VersenyzoSzulEv = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            VersenyzoNeme = v[3];
+            EletkorKategoria = v[4];
+            UszasIdo = DateTime.ParseExact(v[5], "HH:mm:ss", CultureInfo.InvariantCulture);
+            ElsoDepoIdo = DateTime.ParseExact(v[6], "HH:mm:ss", CultureInfo.InvariantCulture);
+            KerekparIdo = DateTime.ParseExact(v[7], "HH:mm:ss", CultureInfo.InvariantCulture);
+            MasodikDepoIdo = DateTime.ParseExact(v[8], "HH:mm:ss", CultureInfo.InvariantCulture);
+            FutasIdo = DateTime.ParseExact(v[9], "HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+    }
+}
